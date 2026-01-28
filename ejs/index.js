150b33b91ffc94657884e8ba19e9fe2c80d448c9
Vaishnavi Kale <vaishnavikale06032006@gmail.com>
2026-01-28 23:51:45 +0530

    Implement dynamic Instagram-like profile rendering using Express and EJS

---
+++ ejs/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+
+const port = 8080;
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.render("home.ejs");
+});
+
+const instaData = require("./data.json");
+
+// Route to display Instagram-like profile based on username 
+app.get("/ig/:username", (req, res) => {
+    let {username} = req.params;
+    let data = instaData[username];
+    if (data) {
+        res.render("instagram.ejs", {data});
+    }else {
+        res.render("error.ejs");
+    }
+});
+
+app.listen(port, () => {
+    console.log("Server is on");
+});
