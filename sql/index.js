4f63cd295574092d6e3e723892d89dd67284fe7a
Vaishnavi Kale <vaishnavikale06032006@gmail.com>
2026-02-10 17:18:13 +0530

    Create a route to display number of users

--- sql/index.js
+++ sql/index.js
@@ -1,5 +1,11 @@
 const { faker } = require('@faker-js/faker');
 const mysql = require("mysql2");
+const express = require("express");
+const path = require("path");
+
+let app = express();
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
 
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -8,31 +14,34 @@ const connection = mysql.createConnection({
   password: 'vaishnavi@12'
 });
 
-let q = "INSERT INTO user (id, user, email, password) VALUES ?";
-let user = [
-         ["456", "456_user", "def@gmail.com", "def"],
-         ["789", "789_user", "ghi@gmail.com", "ghi"],
-];
-
-try {
-    connection.query(q, [user], (err, result) => {
-        if (err) throw err;
-        console.log(result);
-    });
-}catch {
-    console.log(err);
-}
-
-connection.end();
-
 let createRandomUser = () => {
-  return {
-    userId: faker.string.uuid(),
-    username: faker.internet.username(),
-    email: faker.internet.email(),
-    avatar: faker.image.avatar(),
-    password: faker.internet.password(),
-    birthdate: faker.date.birthdate(),
-    registeredAt: faker.date.past(),
-  };
+  return [
+        faker.string.uuid(),
+        faker.internet.username(),
+        faker.internet.email(),
+        faker.internet.password(),
+  ];
 }
+
+app.get("/", (req, res) => {
+  let q = "SELECT count(*) FROM user";
+  try {
+    connection.query(q, (err, result) => {
+        if (err) throw err;
+        let count = result[0]["count(*)"];
+        res.render("home.ejs", {count});
+     });
+  }catch {
+      res.send("Some err in DB");
+      console.log(err);
+    }
+});
+
+
+// connection.end();
+//   res.send("welcome to home page");
+// });
+
+app.listen("8080", () => {
+  console.log("Server is on!!");
+});
